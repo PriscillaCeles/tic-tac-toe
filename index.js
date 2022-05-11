011be79782d3c68f6a6241b833a84d924c75dcbc
Priscilla Celes Lima <pri.c.maciel@hotmail.com>
2022-05-10 21:43:41 -0300

    Update index.js
    refaturação do JS

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const $boardField = document.querySelectorAll(".game-box__moves-board--item");
-const $roundMovesWinner = document.querySelector(".start-box__round-moves");
+//#region CONSTANTS
+const $boardFieldList = document.querySelectorAll(".game-box__moves-board--item");
 
 const $player1Name = document.querySelector(".player--input-player-1");
 const $player2Name = document.querySelector(".player--input-player-2");
 
+const $roundMovesWinner = document.querySelector(".start-box__round-moves");
 const $winnerPrint = document.querySelector(".start-box__moves-winner--name");
 
 const $startButton = document.querySelector(".start-box__button--play");
-const $resettButton = document.querySelector(".start-box__button--reset");
+const $resetButton = document.querySelector(".start-box__button--reset");
 
 const $scorePlayer1 = document.querySelector(".scoreboard-score-1");
 const $scorePlayer2 = document.querySelector(".scoreboard-score-2");
-
 const $playerRound = document.querySelector(".game-box__scoreboard--player");
 
 const $switcher = document.querySelector(".start-box__player--input-switcher");
@@ -19,17 +19,32 @@ const $switcher2 = document.querySelector(
   ".start-box__player--input-switcher-2"
 );
 
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+//#endregion
+
+//#region VARIABLES
 let movePlayer = "X";
 
 let gameStart = false;
 
 let score1 = 0;
 let score2 = 0;
+//#endregion
 
+//#region FUNCTIONS
 function resetBoard() {
-  $boardField[0].textContent = "";
-  for (let i = 0; i < $boardField.length; i++) {
-    $boardField[i].textContent = "";
+  $boardFieldList[0].textContent = "";
+  for (let i = 0; i < $boardFieldList.length; i++) {
+    $boardFieldList[i].textContent = "";
   }
 }
 
@@ -67,78 +82,63 @@ function printScore() {
 }
 
 function verifyGame() {
-  if (
-    ($boardField[0].textContent != "" &&
-      $boardField[0].textContent == $boardField[2].textContent &&
-      $boardField[1].textContent == $boardField[2].textContent) ||
-    ($boardField[3].textContent != "" &&
-      $boardField[3].textContent == $boardField[4].textContent &&
-      $boardField[4].textContent == $boardField[5].textContent) ||
-    ($boardField[6].textContent != "" &&
-      $boardField[6].textContent == $boardField[7].textContent &&
-      $boardField[7].textContent == $boardField[8].textContent) ||
-    ($boardField[0].textContent != "" &&
-      $boardField[0].textContent == $boardField[3].textContent &&
-      $boardField[3].textContent == $boardField[6].textContent) ||
-    ($boardField[1].textContent != "" &&
-      $boardField[1].textContent == $boardField[4].textContent &&
-      $boardField[4].textContent == $boardField[7].textContent) ||
-    ($boardField[2].textContent != "" &&
-      $boardField[2].textContent == $boardField[5].textContent &&
-      $boardField[5].textContent == $boardField[8].textContent) ||
-    ($boardField[0].textContent != "" &&
-      $boardField[0].textContent == $boardField[4].textContent &&
-      $boardField[4].textContent == $boardField[8].textContent) ||
-    ($boardField[2].textContent != "" &&
-      $boardField[2].textContent == $boardField[4].textContent &&
-      $boardField[4].textContent == $boardField[6].textContent)
-  ) {
-    return movePlayer;
-  }
+  let filledFields = 0;
 
-  if (
-      $boardField[0].textContent != "" &&
-      $boardField[1].textContent != "" &&
-      $boardField[2].textContent != "" &&
-      $boardField[3].textContent != "" &&
-      $boardField[4].textContent != "" &&
-      $boardField[5].textContent != "" && 
-      $boardField[6].textContent != '' && 
-      $boardField[7].textContent != '' && 
-      $boardField[8].textContent != ''
-    ) {
+  for (const condition of winConditions) {
+    const fieldIndex0 = condition[0]
+    const fieldIndex1 = condition[1]
+    const fieldIndex2 = condition[2]
 
-    return "draw";
-  }
+    const $field0 = $boardFieldList[fieldIndex0]
+    const $field1 = $boardFieldList[fieldIndex1]
+    const $field2 = $boardFieldList[fieldIndex2]
+
+    if (
+      $field0.textContent != "" &&
+      $field0.textContent == $field1.textContent &&
+      $field1.textContent == $field2.textContent
+      ){
+        return movePlayer;
+      } 
+    }
+
+    for (const $field of $boardFieldList) {
+      if ($field.textContent != '') filledFields++
+    }
+
+    if (filledFields == 9) return 'draw'
 }
 
-$boardField.forEach(function ($field) {
+function result() {
+  const gameResult = verifyGame();
+
+  if (gameResult != undefined && gameResult == "X") {
+    $winnerPrint.innerHTML = $player1Name.value;
+    score1 += 1;
+    setTimeout(resetBoard, 800);
+    movePlayer = "O";
+  } else if (gameResult != undefined && gameResult == "O") {
+    $winnerPrint.innerHTML = $player2Name.value;
+    score2 += 1;
+    setTimeout(resetBoard, 800);
+    movePlayer = "O";
+  }
+  if (gameResult == "draw"){
+    setTimeout(resetBoard, 800);
+    movePlayer = "O";
+    $winnerPrint.innerHTML = 'Empate';
+  }
+}
+//#endregion
+
+$boardFieldList.forEach(function ($field) {
   $field.addEventListener("click", function () {
     if (gameStart) {
-      if ($field.innerHTML != "") {
-        return;
-      }
+      if ($field.innerHTML != "") return;
 
       $field.innerHTML = movePlayer;
-      const gameResult = verifyGame();
 
-      if (gameResult != undefined && gameResult == "X") {
-        $winnerPrint.innerHTML = $player1Name.value;
-        score1 += 1;
-        setTimeout(resetBoard, 800);
-        movePlayer = "O";
-      } else if (gameResult != undefined && gameResult == "O") {
-        $winnerPrint.innerHTML = $player2Name.value;
-        score2 += 1;
-        setTimeout(resetBoard, 800);
-        movePlayer = "O";
-      }
-
-      if (gameResult == "draw"){
-        setTimeout(resetBoard, 800);
-        movePlayer = "O";
-        $winnerPrint.innerHTML = 'Empate';
-      }
+      result ();
       printScore();
       toggleMove();
     }
